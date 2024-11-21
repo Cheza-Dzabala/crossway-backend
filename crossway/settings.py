4d8611cbe443cc2fca33cf77b637e794311d483d
Macheza Dzabala <dzabalamacheza@gmail.com>
2024-11-21 22:42:08 +0200

    print allowed hosts

--- crossway/settings.py
+++ crossway/settings.py
@@ -22,6 +22,8 @@ load_dotenv()
 
 allowedHosts = os.getenv('ALLOWED_HOSTS').split(' ')
 
+print(allowedHosts)
+
 DB_NAME=os.getenv('DB_NAME')
 DB_USER=os.getenv('DB_USER')
 DB_PASSWORD=os.getenv('DB_PASSWORD')
